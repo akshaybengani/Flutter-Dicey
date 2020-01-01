b63636c1aae32f3c1e934e72165783404db51b29
Akshay Bengani <akshaybengani@gmail.com>
2020-01-02 01:40:36 +0530

    Project Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,20 +7,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Dice',
-      home: MyHomeApp(),
-    );
+        title: 'Flutter Dice',
+        home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            centerTitle: true,
+            backgroundColor: Colors.red[800],
+          ),
+          body: MyHomeApp(),
+        ));
   }
 }
 
-class MyHomeApp extends StatelessWidget {
+class MyHomeApp extends StatefulWidget {
+  @override
+  _MyHomeAppState createState() => _MyHomeAppState();
+}
+
+class _MyHomeAppState extends State<MyHomeApp> {
+  int dice1 = Random().nextInt(6) + 1;
+  int dice2 = Random().nextInt(6) + 1;
+  int ball = Random().nextInt(5) + 1;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Dice'),
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                ball = Random().nextInt(5) + 1;
+              });
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              alignment: Alignment.center,
+              child: Image.asset('images/ball$ball.png'),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                dice1 = Random().nextInt(6) + 1;
+                dice2 = Random().nextInt(6) + 1;
+              });
+            },
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Image.asset('images/dice$dice1.png'),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: Image.asset('images/dice$dice2.png'),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Column(),
     );
   }
 }
